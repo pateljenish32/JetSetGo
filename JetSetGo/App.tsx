9bceba66686d4fdb08965cdde7f0e8776e083272
pateljenish32 <pateljenish578@gmail.com>
2024-02-06 18:45:30 +0530

    Basic Structure

---
+++ JetSetGo/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}></View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
